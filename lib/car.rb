0b41f7a779df98db9fa49c6c1bef837fa9837c88
Dan Foley <dan@dan-foley.com>
2020-02-06 17:08:00 +0000

    Done.

--- lib/car.rb
+++ lib/car.rb
@@ -1,1 +1,12 @@
 require_relative "./vehicle.rb"
+# Car class will need access to the Vehicle class/file
+
+class Car < Vehicle
+
+  def go
+    "VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!"
+  end
+  
+
+
+end
