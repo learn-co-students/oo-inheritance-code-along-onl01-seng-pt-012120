5b64f8874961a56c7cfeb8c2ce13fdc17cdf6a1c
Jennifer Beaver <jen.lee.beaver@gmail.com>
2020-02-21 13:15:28 +0000

    Done.

--- lib/car.rb
+++ lib/car.rb
@@ -1,1 +1,16 @@
-require_relative "./vehicle.rb"
+#steps to define the subclass
+
+require_relative "./vehicle.rb" #allows us to access
+                                #the parent class
+
+class Car < Vehicle #here we are defining the class.
+                    #The < inherits Car from Vehicle
+                    #and inherits all of its methods.
+  def go
+    "VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!"
+  end #this code overwrites the parent code
+end
+
+#essentially the compiler will go through our
+#parent class to look for methods and then
+#will go to our subclass  
